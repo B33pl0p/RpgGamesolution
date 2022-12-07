586d99057639c49793445ac59bb7462058a83622
Biplop Giri <biplopgiri120@outlook.com>
2022-12-07 19:04:21 +0545

    initial changes

---
+++ game_engine/Models/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_engine.Models
+{
+    public class Player
+    { 
+       public string? Name { get; set; }
+       public string? CharacterClass { get; set; }
+       public  int Hitponts { get; set; }
+       public  int ExperiencePoints { get; set; }
+       public  int Level { get; set; }
+       public int Gold { get; set; }
+
+    }    
+}
+
+
+
---
+++ game_engine/ViewModel/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using game_engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace game_engine.ViewModel
+{
+    internal class GameSession
+    {
+        Player? CurrentPlayer { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Biplop";
+            CurrentPlayer.Gold = 10000;
+        }
+    }
+}
